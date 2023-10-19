eab53073ef37fe4b54f45b2309538368a93431bd
Albert Valdes <albert.anthony1999@gmail.com>
2023-10-19 02:06:34 -0500

    fix: removed netlify folder from git ignore

---
+++ .netlify/functions/getMovies.ts
@@ -0,0 +1,39 @@
+import type { Context } from "@netlify/functions"
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VITE_BASE_API_URL;
+axios.defaults.headers.common['Authorization'] = process.env.VITE_API_AUTH_TOKEN;
+
+async function getMovies() {
+  try {
+    const resp = await axios.get(`movie/popular/?api_key=${process.env.VITE_API_KEY}`);
+    return resp;
+  } catch {
+    throw new Error('GET Failed');
+  }
+}
+
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+exports.handler = async (req: Request, context: Context) => {
+    try {
+      const data = await getMovies();
+  
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data.data),
+      }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: err.message,
+        })
+      }
+    }
+  }
